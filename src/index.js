d10fee4635f8867565c9ee1a61af51a854c7eb48
anggifergian <anggyfergian56@gmail.com>
2020-10-18 10:41:32 +0700

    Create table

--- src/index.js
+++ src/index.js
@@ -1,10 +1,53 @@
 import React, { Component } from "react";
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.css";
+import { getMovies } from "./services/fakeMovieService";
 
 class App extends Component {
-    render() {
-        return <h1>Hello World</h1>
-    }
+  state = {
+    movies: getMovies(),
+  };
+
+  render() {
+    const { movies } = this.state;
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <table className="table table-hover">
+              <thead>
+                <tr>
+                  <th>Title</th>
+                  <th>Genre</th>
+                  <th>Stock</th>
+                  <th>Rate</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {movies.map((movie) => {
+                  return (
+                    <tr key={movie._id}>
+                      <td>{movie.title}</td>
+                      <td>{movie.genre.name}</td>
+                      <td>{movie.numberInStock}</td>
+                      <td>{movie.dailyRentalRate}</td>
+                      <td>
+                        <button className="btn btn-outline-warning">
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
