a9a38150bfbef975f4c6314e57f6c7c0e9264e4b
anggifergian <anggyfergian56@gmail.com>
2020-10-28 10:50:43 +0700

    Refactoring code

--- src/index.js
+++ src/index.js
@@ -1,67 +1,7 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.css";
-import { getMovies } from "./services/fakeMovieService";
-
-import Pengajar from "./components/counter";
-
-class App extends Component {
-  state = {
-    movies: getMovies(),
-  };
-
-  listingMovies = () => {
-    const { movies } = this.state;
-    return movies.map((movie) => {
-      return (
-        <tr key={movie._id}>
-          <td>{movie.title}</td>
-          <td>{movie.genre.name}</td>
-          <td>{movie.numberInStock}</td>
-          <td>{movie.dailyRentalRate}</td>
-          <td>
-            <button className="btn btn-outline-warning">Delete</button>
-          </td>
-        </tr>
-      );
-    });
-  };
-
-  stateTheNumberOfMovies = () => {
-    const { movies } = this.state;
-    return (
-      <p>
-        Showing {movies.length === 0 ? "0" : movies.length} movies in the
-        database.
-      </p>
-    );
-  };
-
-  render() {
-    return (
-      <div className="container mt-4">
-        <div className="row">
-          <div className="col-md-6">
-            <Pengajar />
-            <div>{this.stateTheNumberOfMovies()}</div>
-            <table className="table table-hover">
-              <thead>
-                <tr>
-                  <th>Title</th>
-                  <th>Genre</th>
-                  <th>Stock</th>
-                  <th>Rate</th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>{this.listingMovies()}</tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+import App from "../src/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
